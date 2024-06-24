73b0c9a10021248d7b5892b3e1a721e91393f95d
Sameer <sameersattar1111@gmail.com>
2024-06-24 15:36:03 +0500

    complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart";
+import Shop from "./pages/Shop";
+import LoginSignup from "./pages/LoginSignup";
+import Product from "./pages/Product";
+import ShopCategory from "./pages/ShopCategory";
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category={"mens"} />} />
+          <Route path="/womens" element={<ShopCategory category={"womens"} />} />
+          <Route path="/kids" element={<ShopCategory category={"kids"} />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import "./Navbar.css"
+import React, { useState } from "react";
+import cart from "../../assets/cart_icon.png"
+import logo from "../../assets/logo.png"
+import "./Navbar.css";
 
 const Navbar = () => {
+    const [menu, setMenu] = useState("shop")
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={() => setMenu("shop")}>SHOP {menu === 'shop' ?   <hr/> : "" }</li>
+        <li onClick={() => setMenu("mens")}>MEN {menu === 'mens' ?   <hr/> : "" }</li>
+        <li onClick={() => setMenu("womens")}>WOMEN {menu === 'womens' ?   <hr/> : "" }</li>
+        <li onClick={() => setMenu("kids")}>KIDS {menu === 'kids' ?   <hr/> : "" }</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart} alt="" />
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
