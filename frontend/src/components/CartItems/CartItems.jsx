936f3ea14cf2264824559440f404c4794a30f08f
Sameer <sameersattar1111@gmail.com>
2024-06-29 23:18:52 +0500

    half complete

--- frontend/src/components/CartItems/CartItems.jsx
+++ frontend/src/components/CartItems/CartItems.jsx
@@ -4,8 +4,7 @@ import { ShopContext } from "../../Context/ShopContext";
 import "./CartItems.css";
 
 const CartItems = () => {
-  const { all_product, cartItems, addToCart, removeFromCart } =
-    useContext(ShopContext);
+  const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
   return (
     <div className="cartItems">
       <div className="cartItems_format_main">
@@ -21,15 +20,16 @@ const CartItems = () => {
         if (cartItems[e.id] > 0) {
           return (
             <div>
-              <div className="cartItems_format">
-                <img src={e.image} alt="" />
+              <div className="cartItems_format cartItems_format_main">
+                <img src={e.image} alt="" className="cartIcon_product_icon" />
                 <p>{e.name}</p>
-                <p>{e.new_price}</p>
+                <p>${e.new_price}</p>
                 <button className="cartItems_quantity">
                   {cartItems[e.id]}
                 </button>
-                <p>{e.new_price * cartItems[e.id]}</p>
+                <p>${e.new_price * cartItems[e.id]}</p>
                 <img
+                  className="cartIcon_remove_icon"
                   src={removeIcon}
                   onClick={() => removeFromCart(e.id)}
                   alt=""
@@ -38,10 +38,38 @@ const CartItems = () => {
               <hr />
             </div>
           );
-        } else {
-          <h1>There is no product</h1>;
         }
+        return null;
       })}
+      <div className="cartItems_down">
+        <div className="cartItems_total">
+          <h1>Cart Totals</h1>
+          <div>
+            <div className="cartItems_total_Items">
+              <p>Subtotals</p>
+              <p>${0}</p>
+            </div>
+            <hr />
+            <div className="cartItems_total_Items">
+              <p>Shopping Fees</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartItems_total_Items">
+              <p>Total</p>
+              <p>${0}</p>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartItems_promocode">
+          <p>If you have a promo code, Enter it here</p>
+          <div className="cartItems_promobox">
+            <input type="text" placeholder="promo code" />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
